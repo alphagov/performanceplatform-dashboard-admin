d049cf853cdd8eb99faeab869d26bf025209328a
Tom Booth <tombooth@gmail.com>
2014-06-02 11:20:13 +0100

    Get info from GOV.UK start pages

---
+++ src/govuk.js
@@ -0,0 +1,38 @@
+
+var request = require('request');
+
+
+function GovUK() {
+}
+
+GovUK.prototype.fromStartPage = function(url, callback) {
+  request.get(url + '.json', function(err, response, body) {
+    if (err) callback(err);
+    else if (response.statusCode < 200 || response.statusCode >= 300) {
+      callback('failed with status: ' + response.statusCode);
+    } else {
+      var json = JSON.parse(body);
+      callback(null, {
+        title: json.title,
+        description: json.details.description,
+        start_page: {
+          url: url,
+          title: json.title
+        },
+        links: json.related.map(function(link) {
+          return {
+            url: link.web_url,
+            title: link.title
+          };
+        })
+      });
+    }
+  });
+};
+
+GovUK.fromConfig = function(config) {
+  return new GovUK(
+  );
+};
+
+module.exports = GovUK;
