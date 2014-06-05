cec09c0f1c731a8457ee87d26c959cdfbe52cddc
Tom Booth <tombooth@gmail.com>
2014-06-05 13:00:51 +0100

    Allow the configuration of the tx modules

---
+++ src/module_helper.js
@@ -0,0 +1,65 @@
+
+var mustache = require('mustache'),
+    fs = require('fs');
+
+function ModuleHelper(modules) {
+  this.modules = modules;
+}
+
+ModuleHelper.prototype.generate = function(existingModules, form) {
+
+  existingModules = existingModules || [];
+
+  var slugsToRemove = _.flatten(this.modules.map(function(m) {
+    return m.built ? m.slugs : [];
+  }));
+
+  var remainingModules = existingModules.filter(function(m) {
+    return slugsToRemove.indexOf(m.slug) < 0;
+  });
+
+  var newModules = _.flatten(this.modules.map(function(m) {
+    var moduleConfig = [];
+    if (m.built) {
+      moduleConfig = m.templates.map(function(template) {
+        var templateContent = fs.readFileSync('./templates/' + template).toString('utf8'),
+            rendered = mustache.render(templateContent, { form: form });
+
+        return JSON.parse(rendered);
+      });
+    }
+    return moduleConfig;
+  }));
+
+  return newModules.concat(remainingModules);
+
+};
+
+ModuleHelper.prototype.parse = function(dashboard) {
+  return this.modules.map(function(module) {
+
+    if (dashboard.modules) {
+      module.existing = _.flatten(module.slugs.map(function(slug) {
+        var matched = dashboard.modules.filter(function(dashboardModule) {
+          return dashboardModule.slug === slug;
+        });
+
+        return matched;
+      }));
+
+      module.enabled = module.existing.length > 0 && module.existing.every(function(m) { return !m.disabled });
+    } else {
+      module.existing = [ ];
+      module.enabled = false;
+    }
+
+    return module;
+  });
+};
+
+ModuleHelper.prototype.getTxIdentifier = function(module) {
+  return module.existing.length > 0 ? module.existing[0]['query-params']['filter_by'][0].split(':')[1] : '';
+};
+
+module.exports = ModuleHelper;
+
